69761eec4c89f459836ae3c73f20595c0eca4872
Ryan Sponzilli <ryguy2k4@gmail.com>
2024-10-14 21:33:36 -0500

    commit

--- assignment2/integration1.sql
+++ assignment2/integration1.sql
@@ -1,4 +1,5 @@
 INSERT INTO Integrated_Arrival
+
 SELECT 'A1' as Airline, 
         b.FlightNumber as FlightNumber,
         a.ArrivalDate as ScheduledArrivalDate, 
@@ -8,7 +9,15 @@ SELECT 'A1' as Airline,
         c.GateTime as GateTime, 
         c.LandingTime as LandingTime,
         a.ArrivalGate as ArrivalGate
+
 FROM Airline1_Flight as a
-JOIN Airline1_Schedule as b ON a.FlightId = b.FlightId
-JOIN Airport3_Arrivals as c ON b.FlightNumber = c.FlightNumber AND a.ArrivalDate = c.Scheduled AND 'A1' = c.Airline AND 'EWR' = b.ArrivalAirport
+
+JOIN Airline1_Schedule as b 
+ON a.FlightId = b.FlightId
+
+JOIN Airport3_Arrivals as c 
+ON 'EWR' = b.ArrivalAirport
+AND 'A1' = c.Airline 
+AND b.FlightNumber = c.FlightNumber 
+AND a.ArrivalDate = c.Scheduled 
 ;
