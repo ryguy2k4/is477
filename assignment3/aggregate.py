7323d0ca55cbd7ddc33810881939f928d2669b71
Ryan Sponzilli <ryguy2k4@gmail.com>
2024-11-14 15:39:51 -0600

    finished

--- assignment3/aggregate.py
+++ assignment3/aggregate.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("input/Food_Inspections_50k.csv")
-df = pd.DataFrame(df.value_counts("Facility Type")).reset_index()
-df = df.rename(columns={"Facility Type": "Facility_Type", "count": "Count"})
-df = df.sort_values(["Count", "Facility_Type"], ascending=True)
+df = pd.read_csv("/Users/ryansponzilli/Developer/UIUC/is477/is477-fa24-ryants4/assignment3/input/Food_Inspections_50k.csv")
+df = df.groupby("Facility Type", dropna=False).agg('count').reset_index()
+df = df.rename(columns={"Facility Type": "Facility_Type", "Inspection ID": "Count"})
+df = df[["Facility_Type", "Count"]]
+df = df.sort_values(["Count","Facility_Type"], ascending=True)
+
 df.to_csv("results/pandas-fac-types.csv", index=False)
--- assignment3/compare.py
+++ assignment3/compare.py
@@ -3,5 +3,5 @@ import pandas as pd
 df_pandas = pd.read_csv("results/pandas-fac-types.csv")
 df_sql = pd.read_csv("results/sql-fac-types.csv")
 
-compare = df_pandas.compare(df_sql[0:311])
+compare = df_pandas.compare(df_sql)
 compare.to_csv("results/comparison.csv", index=True)
